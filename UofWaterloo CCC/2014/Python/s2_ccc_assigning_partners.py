ae86289f46295bf86d03ca2dd3845592c2060df1
GameGenesis <gamegenesisdev@gmail.com>
2022-02-08 18:29:17 -0500

    Update 2014 S2

--- UofWaterloo CCC/2014/Python/s2_ccc_assigning_partners.py
+++ UofWaterloo CCC/2014/Python/s2_ccc_assigning_partners.py
@@ -2,8 +2,8 @@
 
 number_of_students = int(input())
 
-member_names_1 = input().split(" ")
-member_names_2 = input().split(" ")
+member_names_1 = input().split()
+member_names_2 = input().split()
 members = []
 
 match = True
@@ -13,11 +13,11 @@ for i in range(number_of_students):
         members.append([member_names_1[i], member_names_2[i]])
 
     for j in range(len(members)):
-        if member_names_1[i] in members[j] or member_names_2[i] in members[j]:
-            if member_names_1[i] == member_names_2[i]:
+        if member_names_1[i] == member_names_2[i]:
                 match = False
                 break
-            
+        
+        if member_names_1[i] in members[j] or member_names_2[i] in members[j]:
             if member_names_1[i] in members[j] and member_names_2[i] in members[j]:
                 break
             else:
